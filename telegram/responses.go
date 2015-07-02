0b771dc37e155766b3f3e8f0e5015b850020210b
Ilya Siganov <ilya.blan4@gmail.com>
2015-07-02 23:48:16 +0600

    move telegram specific functions to package

---
+++ telegram/responses.go
@@ -0,0 +1,123 @@
+package telegram
+
+import "net/http"
+
+// Response is telegram base response envelop
+type Response struct {
+	Response *http.Response
+	Ok       bool        `json:"ok"`
+	Result   interface{} `json:"result"`
+}
+
+// User struct represents a Telegram user or bot.
+type User struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name,omitempty"`
+	Username  string `json:"username,omitempty"`
+}
+
+// GroupChat struct represents a group chat.
+type GroupChat struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+}
+
+// Message struct represents a message.
+type Message struct {
+	MessageID           int         `json:"message_id"`
+	From                User        `json:"from"`
+	Date                int         `json:"date"`
+	Chat                interface{} `json:"chat"`
+	ForwardFrom         User        `json:"forward_from,omitempty"`
+	ForwardDate         int         `json:"forward_date,omitempty"`
+	ReplyToMessage      *Message    `json:"reply_to_message,omitempty"`
+	Text                string      `json:"text,omitempty"`
+	Audio               Audio       `json:"audio,omitempty"`
+	Document            Document    `json:"document,omitempty"`
+	Photo               []PhotoSize `json:"photo,omitempty"`
+	Sticker             Sticker     `json:"sticker,omitempty"`
+	Video               Video       `json:"video,omitempty"`
+	Contact             Contact     `json:"contact,omitempty"`
+	Location            Location    `json:"location,omitempty"`
+	NewChatParticipant  User        `json:"new_chat_participant,omitempty"`
+	LeftChatParticipant User        `json:"left_chat_participant,omitempty"`
+	NewChatTitle        string      `json:"new_chat_title,omitempty"`
+	NewChatPhoto        []PhotoSize `json:"new_chat_photo,omitempty"`
+	DeleteChatPhoto     bool        `json:"delete_chat_photo,omitempty"`
+	GroupChatCreated    bool        `json:"group_chat_created,omitempty"`
+}
+
+// PhotoSize struct represents one size of a photo or a file / sticker thumbnail.
+type PhotoSize struct {
+	FileID   string `json:"file_id"`
+	Width    int    `json:"width"`
+	Height   int    `json:"height"`
+	FileSize int    `json:"file_size,omitempty"`
+}
+
+// Audio struct represents an audio file (voice note).
+type Audio struct {
+	FileID   string `json:"file_id"`
+	Duration int    `json:"duration"`
+	MimeType string `json:"mime_type,omitempty"`
+	FileSize int    `json:"file_size,omitempty"`
+}
+
+// Document struct represents a general file (as opposed to photos and audio files).
+type Document struct {
+	FileID   string    `json:"file_id"`
+	Thumb    PhotoSize `json:"thumb"`
+	FileName string    `json:"file_name,omitempty"`
+	MimeType string    `json:"mime_type,omitempty"`
+	FileSize int       `json:"file_size,omitempty"`
+}
+
+// Sticker struct
+type Sticker struct {
+	FileID   string    `json:"file_id"`
+	Width    int       `json:"width"`
+	Height   int       `json:"heiht"`
+	Thumb    PhotoSize `json:"thumb"`
+	FileSize int       `json:"file_size,omitempty"`
+}
+
+// Video struct represents a video file.
+type Video struct {
+	FileID   string    `json:"file_id"`
+	Width    int       `json:"width"`
+	Height   int       `json:"height"`
+	Duration int       `json:"duration"`
+	Thumb    PhotoSize `json:"thumb"`
+	MimeType string    `json:"mime_type,omitempty"`
+	FileSize int       `json:"fil_size,omitempty"`
+	Caption  string    `json:"caption,omitempty"`
+}
+
+// Contact struct represents a phone contact.
+type Contact struct {
+	PhoneNumber string `json:"phone_number"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name,omitempty"`
+	UserID      string `json:"user_id,omitempty"`
+}
+
+// UserProfilePhotos struct represents a user's profile pictures.
+type UserProfilePhotos struct {
+	TotalCount int         `json:"total_count"`
+	Photos     []PhotoSize `json:"photos"`
+}
+
+// Location struct represents a point on the map.
+type Location struct {
+	Longitude float32 `json:"longitude"`
+	Latitude  float32 `json:"latitude"`
+}
+
+// ReplyKeyboardMarkup struct represents a custom keyboard with reply options.
+type ReplyKeyboardMarkup struct {
+	Keyboard        [][]string `json:"keyboard"`
+	ResizeKeyboard  bool       `json:"resize_keyboard,omitempty"`
+	OneTimeKeyboard bool       `json:"one_time_keyboard,omitempty"`
+	Selective       bool       `json:"selective,omitempty"`
+}
---
+++ telegram/telegram.go
@@ -0,0 +1,172 @@
+package telegram
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"image"
+	"image/jpeg"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+// BotURL is url to telegram bot api
+const BotURL string = "https://api.telegram.org/bot"
+
+// Telegram is struct to store client data
+type Telegram struct {
+	Key    string
+	client *http.Client
+	botURL string
+}
+
+// NewTelegram is factory method for Telegram struct
+func NewTelegram(httpClient *http.Client, key string) *Telegram {
+	return &Telegram{
+		Key:    key,
+		client: httpClient,
+		botURL: fmt.Sprintf("https://api.telegram.org/bot%v/", key),
+	}
+}
+
+// do is helper function to send requests
+func (telegram Telegram) do(req *http.Request, v interface{}) (*Response, error) {
+	log.Printf("Sending request: %v", req)
+	resp, err := telegram.client.Do(req)
+	if err != nil {
+		return &Response{}, err
+	}
+
+	defer resp.Body.Close()
+
+	r := &Response{Response: resp}
+	if v != nil {
+		r.Result = v
+		err = json.NewDecoder(resp.Body).Decode(r)
+	}
+
+	return r, err
+}
+
+// GetMe A simple method for testing your bot's auth token.
+// Requires no parameters.
+// Returns basic information about the bot in form of a User object.
+func (telegram Telegram) GetMe() (*User, error) {
+	uri := telegram.botURL + "getMe"
+	req, err := http.NewRequest("GET", uri, bytes.NewBufferString(""))
+	if err != nil {
+		return &User{}, err
+	}
+
+	user := new(User)
+	_, err = telegram.do(req, user)
+
+	return user, err
+}
+
+// SendMessage - Use this method to send text messages
+func (telegram Telegram) SendMessage(chatID int, text string, disableWebPagePreview bool, replyToMessageID int, replyMarkup *ReplyKeyboardMarkup) (*Message, error) {
+	msg := new(Message)
+	uri := telegram.botURL + "sendMessage"
+	query := url.Values{}
+	query.Set("text", text)
+	query.Add("chat_id", strconv.Itoa(chatID))
+	//query.Add("disable_web_page_preview", strconv.FormatBool(disableWebPagePreview))
+	if replyToMessageID != 0 {
+		query.Add("reply_to_message_id", strconv.Itoa(replyToMessageID))
+	}
+	if replyMarkup != nil {
+		bReplyMarkup, err := json.Marshal(replyMarkup)
+		if err != nil {
+			return msg, err
+		}
+		query.Add("reply_markup", string(bReplyMarkup))
+	}
+	uri = uri + "?" + query.Encode()
+	req, err := http.NewRequest("POST", uri, bytes.NewBufferString(""))
+	req.Header.Add("Content-Length", strconv.Itoa(len(query.Encode())))
+	_, err = telegram.do(req, msg)
+	return msg, err
+}
+
+// SendPhoto - Use this method to send photos.
+func (telegram Telegram) SendPhoto(chatID int, photo image.Image, caption string, replyToMessageID int, replyMarkup *ReplyKeyboardMarkup) (*Message, error) {
+	var (
+		uri          = telegram.botURL + "sendPhoto"
+		imageQuality = jpeg.Options{Quality: jpeg.DefaultQuality}
+		b            bytes.Buffer
+		fw           io.Writer
+		msg          = new(Message)
+		err          error
+	)
+	w := multipart.NewWriter(&b)
+	if fw, err = w.CreateFormField("chat_id"); err != nil {
+		return msg, err
+	}
+	if _, err = fw.Write([]byte(strconv.Itoa(chatID))); err != nil {
+		return msg, err
+	}
+	if caption != "" {
+		if fw, err = w.CreateFormField("caption"); err != nil {
+			return msg, err
+		}
+		if _, err = fw.Write([]byte(caption)); err != nil {
+			return msg, err
+		}
+	}
+	if replyToMessageID != 0 {
+		if fw, err = w.CreateFormField("reply_to_message_id"); err != nil {
+			return msg, err
+		}
+		if _, err = fw.Write([]byte(strconv.Itoa(replyToMessageID))); err != nil {
+			return msg, err
+		}
+	}
+	if replyMarkup != nil {
+		keyboard, err := json.Marshal(replyMarkup)
+		if err != nil {
+			return msg, err
+		}
+		if fw, err = w.CreateFormField("reply_markup"); err != nil {
+			return msg, err
+		}
+		if _, err = fw.Write([]byte(keyboard)); err != nil {
+			return msg, err
+		}
+	}
+	if fw, err = w.CreateFormFile("photo", "image.jpg"); err != nil {
+		return msg, err
+	}
+	if err = jpeg.Encode(fw, photo, &imageQuality); err != nil {
+		return msg, err
+	}
+	w.Close()
+
+	req, err := http.NewRequest("POST", uri, &b)
+	if err != nil {
+		return msg, err
+	}
+	req.Header.Set("Content-Type", w.FormDataContentType())
+	req.Header.Add("Content-Length", strconv.Itoa(b.Len()))
+
+	_, err = telegram.do(req, msg)
+	return msg, err
+}
+
+func (telegram Telegram) sendChatAction(chatID int, action string) (err error) {
+	query := url.Values{}
+	query.Set("action", action)
+	query.Add("chat_id", strconv.Itoa(chatID))
+	uri := telegram.botURL + "sendChatAction" + "?" + query.Encode()
+	req, err := http.NewRequest("POST", uri, bytes.NewBufferString(""))
+	if err != nil {
+		return
+	}
+	req.Header.Add("Content-Length", strconv.Itoa(len(query.Encode())))
+	_, err = telegram.do(req, nil)
+	return
+}
